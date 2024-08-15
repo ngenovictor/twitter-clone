9a06ae6248cc499220d29d7a7e757ac586999a5f
victor ngeno <ngenovictor321@gmail.com>
2024-08-15 17:09:08 +0300

    update the new tweet section

--- lib/feed_panel.dart
+++ lib/feed_panel.dart
@@ -4,9 +4,8 @@ class FeedPanel extends StatelessWidget {
   final BorderSide _borderSide =
       const BorderSide(color: Color.fromRGBO(47, 51, 54, 1), width: 1);
   const FeedPanel({super.key});
-  final isHovering = false;
-  @override
-  Widget build(BuildContext context) {
+
+  Widget getTopPart() {
     return Container(
         decoration: BoxDecoration(
             border: Border(
@@ -56,5 +55,98 @@ class FeedPanel extends StatelessWidget {
             )
           ]),
         ));
+  }
+
+  Widget getNewTweetPart() {
+    return Container(
+        decoration: BoxDecoration(
+            border: Border(
+                bottom: _borderSide, left: _borderSide, right: _borderSide)),
+        child: Padding(
+          padding: const EdgeInsets.all(20),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Image.asset(
+                "lib/assets/profile-pic.jpg",
+                height: 40,
+              ),
+              const SizedBox(
+                width: 5,
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const SizedBox(
+                      height: 50,
+                      child: Text(
+                        "What is happening?!",
+                        style: TextStyle(
+                            fontSize: 20,
+                            color: Color.fromRGBO(113, 118, 123, 1)),
+                      )),
+                  Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        const SizedBox(
+                            width: 180,
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              children: [
+                                Icon(
+                                  Icons.photo_outlined,
+                                  color: Color.fromRGBO(29, 155, 240, 1),
+                                ),
+                                Icon(
+                                  Icons.gif_box_outlined,
+                                  color: Color.fromRGBO(29, 155, 240, 1),
+                                ),
+                                Icon(
+                                  Icons.mediation_rounded,
+                                  color: Color.fromRGBO(29, 155, 240, 1),
+                                ),
+                                Icon(
+                                  Icons.emoji_emotions_outlined,
+                                  color: Color.fromRGBO(29, 155, 240, 1),
+                                ),
+                                Icon(
+                                  Icons.calendar_today_outlined,
+                                  color: Color.fromRGBO(29, 155, 240, 1),
+                                ),
+                                Icon(
+                                  Icons.location_on_outlined,
+                                  color: Color.fromRGBO(29, 155, 240, 1),
+                                )
+                              ],
+                            )),
+                        Container(
+                          width: 566 / 2,
+                          alignment: Alignment.centerRight,
+                          child: ElevatedButton(
+                            onPressed: () {},
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor:
+                                  const Color.fromRGBO(29, 155, 240, 1),
+                              foregroundColor:
+                                  const Color.fromRGBO(92, 92, 92, 1),
+                            ),
+                            child: const Text(
+                              "Post",
+                            ),
+                          ),
+                        )
+                      ])
+                ],
+              )
+            ],
+          ),
+        ));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [getTopPart(), getNewTweetPart()],
+    );
   }
 }
