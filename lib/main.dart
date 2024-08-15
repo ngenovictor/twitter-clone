dde1ab0490635f1e455d4eb73ed33354971e4057
victor ngeno <ngenovictor321@gmail.com>
2024-08-15 11:22:01 +0300

    change default text style

--- lib/main.dart
+++ lib/main.dart
@@ -30,6 +30,7 @@ class Twitter extends StatelessWidget {
         // This works for code too, not just values: Most code changes can be
         // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        fontFamily: "Roboto",
         useMaterial3: true,
       ),
       home: const MyHomePage(title: 'Twitter'),
