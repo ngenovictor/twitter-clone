85cfe51cb1458bc11f16c3921e1bf69a465eef13
victor ngeno <ngenovictor321@gmail.com>
2024-08-16 14:04:14 +0300

    update to make tweet image nullable

--- lib/models/tweet.dart
+++ lib/models/tweet.dart
@@ -1,5 +1,16 @@
 class Tweet {
   String message;
+  String? photoPath;
+  String profilePhotoUrl;
+  String name;
+  String username;
+  String timePosted;
 
-  Tweet.fromJson(Map<String, dynamic> json) : message = json["message"];
+  Tweet.fromJson(Map<String, dynamic> json)
+      : message = json["message"],
+        photoPath = json['photo_path'],
+        profilePhotoUrl = json['profile_photo_url'],
+        name = json['name'],
+        username = json['username'],
+        timePosted = json['time_posted'];
 }
