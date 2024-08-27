2fcb0725031b172589b6039ac1d02895dea93279
victor ngeno <ngenovictor321@gmail.com>
2024-08-27 15:01:03 +0300

    update left panel contents based on the kind of width of device

--- lib/left_panel.dart
+++ lib/left_panel.dart
@@ -3,32 +3,50 @@ import 'package:twitter/constants/colors.dart';
 import 'package:twitter/responsive.dart';
 
 class LeftPanel extends StatelessWidget {
-  final SizedBox _spaceBetween = const SizedBox(
-    height: 10,
-  );
-  final Map<String, IconData> _iconElements = {
-    "Home": Icons.home,
-    "Explore": Icons.search,
-    "Notifications": Icons.blender_outlined,
-    "Messages": Icons.message,
-    "Grok": Icons.home,
-    "Lists": Icons.chrome_reader_mode_outlined,
-    "Bookmarks": Icons.bookmark,
-    "Communities": Icons.people,
-    "Premium": Icons.flash_on,
-    "verified Orgs": Icons.flash_on,
-    "Profile": Icons.person_outline,
-    "More": Icons.more_horiz
-  };
-  final bool isDrawerItems;
+  LeftPanel({super.key});
 
-  LeftPanel({super.key, this.isDrawerItems = false});
+  Map<String, IconData> getIconElements(BuildContext context) {
+    if (!Responsive.isMobile(context)) {
+      return {
+        "Home": Icons.home,
+        "Explore": Icons.search,
+        "Notifications": Icons.blender_outlined,
+        "Messages": Icons.message,
+        "Grok": Icons.home,
+        "Lists": Icons.chrome_reader_mode_outlined,
+        "Bookmarks": Icons.bookmark,
+        "Communities": Icons.people,
+        "Premium": Icons.flash_on,
+        "verified Orgs": Icons.flash_on,
+        "Profile": Icons.person_outline,
+        "More": Icons.more_horiz
+      };
+    } else {
+      return {
+        "Profile": Icons.person_outline,
+        "Premium": Icons.flash_on,
+        "Lists": Icons.chrome_reader_mode_outlined,
+        "Bookmarks": Icons.bookmark,
+        "verified Orgs": Icons.flash_on,
+        "Monetization": Icons.money,
+        "Ads": Icons.open_in_new,
+        "Jobs": Icons.work_outline_outlined,
+        "Settings and Privacy": Icons.settings_outlined,
+        "Logout": Icons.logout_outlined
+      };
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     List<Widget> panelIcons = [];
-    bool showIconText = isDrawerItems || Responsive.isDesktop(context);
-    _iconElements.forEach(
+    bool showIconText = !Responsive.isTablet(context);
+    final SizedBox spaceBetween = Responsive.isMobile(context)
+        ? const SizedBox()
+        : const SizedBox(
+            height: 10,
+          );
+    getIconElements(context).forEach(
       (String iconText, IconData icon) {
         panelIcons.add(
           ElevatedButton(
@@ -49,7 +67,7 @@ class LeftPanel extends StatelessWidget {
                 ),
                 if (showIconText)
                   const SizedBox(
-                    width: 10,
+                    width: 20,
                   ),
                 if (showIconText)
                   Text(
@@ -60,28 +78,96 @@ class LeftPanel extends StatelessWidget {
             ),
           ),
         );
-        panelIcons.add(_spaceBetween);
+        panelIcons.add(spaceBetween);
       },
     );
 
     return Container(
-        margin: const EdgeInsets.only(right: 30),
+        margin: Responsive.isMobile(context)
+            ? const EdgeInsets.only(right: 8)
+            : const EdgeInsets.only(right: 30),
         child: SingleChildScrollView(
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-                  const Image(
-                    image: AssetImage("lib/assets/logo-white.png"),
-                    width: 50,
-                    height: 50,
-                  ),
-                  _spaceBetween,
+                  if (Responsive.isMobile(context))
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Image.asset("lib/assets/profile-pic.jpg",
+                                  width: 30),
+                              const Icon(
+                                Icons.add_circle_outline,
+                                color: Colors.white,
+                              )
+                            ],
+                          ),
+                          const SizedBox(
+                            height: 10,
+                          ),
+                          const Text("Victor",
+                              style:
+                                  TextStyle(fontSize: 16, color: Colors.white)),
+                          Text(
+                            "@victor_veekay",
+                            style: TextStyle(color: CustomColors.twitterGrey),
+                          ),
+                          const SizedBox(
+                            height: 10,
+                          ),
+                          Row(
+                            children: [
+                              Row(
+                                children: [
+                                  const Text("1,206",
+                                      style: TextStyle(
+                                          fontSize: 16, color: Colors.white)),
+                                  const SizedBox(
+                                    width: 5,
+                                  ),
+                                  Text("Following",
+                                      style: TextStyle(
+                                          color: CustomColors.twitterGrey))
+                                ],
+                              ),
+                              const SizedBox(
+                                width: 20,
+                              ),
+                              Row(
+                                children: [
+                                  const Text("541",
+                                      style: TextStyle(
+                                          fontSize: 16, color: Colors.white)),
+                                  const SizedBox(
+                                    width: 5,
+                                  ),
+                                  Text("Followers",
+                                      style: TextStyle(
+                                          color: CustomColors.twitterGrey))
+                                ],
+                              )
+                            ],
+                          )
+                        ],
+                      ),
+                    ),
+                  if (!Responsive.isMobile(context))
+                    const Image(
+                      image: AssetImage("lib/assets/logo-white.png"),
+                      width: 50,
+                      height: 50,
+                    ),
+                  spaceBetween,
                 ] +
                 panelIcons +
                 [
-                  _spaceBetween,
-                  if (Responsive.isMobile(context) ||
-                      Responsive.isDesktop(context))
+                  spaceBetween,
+                  if (Responsive.isDesktop(context))
                     ElevatedButton(
                       onPressed: () {},
                       style: ElevatedButton.styleFrom(
